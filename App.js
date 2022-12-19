fe4dc9faf6a76aac78921bf7b8a1c8166631c6f5
TOMFrontEnd <96675879+TOMFrontEnd@users.noreply.github.com>
2022-12-19 17:34:25 +1000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,41 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {StyleSheet, View, Text, Button, Linking} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.body}>
+      <Text style={styles.text}>Programming with TOM</Text>
+      <Button
+        title="youtube channel"
+        onPress={() => {
+          Linking.openURL('https://www.youtube.com/');
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  body: {
+    flex: 1,
+    backgroundColor: '#0000ff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#ffffff',
+    fontSize: 20,
+    fontStyle: 'italic',
+    margin: 10,
+  },
+});
+
+export default App;
